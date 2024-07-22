723f67a8b6bc13dee86afd3de785a32530ac3011
peomcherry <101077728+peomcherry@users.noreply.github.com>
2024-07-22 13:26:22 +0800

    second commit

---
+++ examples/chassis_ctrl/chassis_ctrl.ino
@@ -0,0 +1,9 @@
+#include "../../include/DJIMotorAlgoESP.hpp"
+//M2006_P36 MOTOR(1);
+void setup() {
+  //can_init(8, 18, 100);
+
+}
+void loop() {
+  //MOTOR.set_location(8192*12);
+}
---
+++ src/DJIMotorAlgoESP.hpp
@@ -0,0 +1,6 @@
+#ifndef _DJIMotorALGOESP_HPP_
+#define _DJIMotorALGOESP_HPP_
+
+
+
+#endif
